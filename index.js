cd1d7d2672ce683012f296d8f7f84d718416d034
Tyler Long <tyler4long@gmail.com>
2018-01-12 20:09:28 +0800

    Update ringcentral-js-concise

--- index.js
+++
@@ -1,8 +0,0 @@
-app.post('/webhook', async (req, res) => {
-  const message = req.body.body
-  if (message && message.creatorId !== rc.token().owner_id) {
-    await sendGlipMessage(message.groupId, JSON.stringify(message, null, 2))
-  }
-  res.set('validation-token', req.get('validation-token'))
-  res.send('')
-})
--- messages/index.js
+++ messages/index.js
@@ -1,20 +1,19 @@
-const RingCentral = require('ringcentral-js-concise')
+import RingCentral from 'ringcentral-js-concise'
 
-const handleMessage = (event, context, callback) => {
+const handleMessage = async (event, context, callback) => {
   const body = JSON.parse(event.body)
   const message = body.body
   const token = JSON.parse(process.env.GLIP_API_TOKEN)
   if (message && message.creatorId !== token.owner_id) {
     const rc = new RingCentral('', '', process.env.GLIP_API_SERVER)
     rc.token(token)
-    rc.post('/restapi/v1.0/glip/posts', {
+    await rc.post('/restapi/v1.0/glip/posts', {
       groupId: message.groupId,
       text: JSON.stringify(message, null, 2),
       attachments: undefined
     })
   }
-  // callback(null, { statusCode: 200, body: event.headers })
-  callback(null, { statusCode: 200, body: JSON.stringify(event.headers, null, 2) })
+  callback(null, { statusCode: 200, body: '', headers: { 'validation-token': event.headers['validation-token'] } })
 }
 
 module.exports = { handleMessage }
