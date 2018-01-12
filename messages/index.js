2dd9dfc190536d1ecd40a3095ea00127a1e41e78
Tyler Long <tyler4long@gmail.com>
2018-01-12 18:11:36 +0800

    Can send message to Glip

--- messages/index.js
+++ messages/index.js
@@ -5,13 +5,13 @@ const handleMessage = (event, context, callback) => {
   const message = body.body
   const token = JSON.parse(process.env.GLIP_API_TOKEN)
   if (message && message.creatorId !== token.owner_id) {
-    // const rc = new RingCentral('', '', process.env.GLIP_API_SERVER)
-    // rc.token(token)
-    // rc.post('/restapi/v1.0/glip/posts', {
-    //   groupId: message.groupId,
-    //   text: JSON.stringify(message, null, 2),
-    //   attachments: undefined
-    // })
+    const rc = new RingCentral('', '', process.env.GLIP_API_SERVER)
+    rc.token(token)
+    rc.post('/restapi/v1.0/glip/posts', {
+      groupId: message.groupId,
+      text: JSON.stringify(message, null, 2),
+      attachments: undefined
+    })
   }
   // callback(null, { statusCode: 200, body: event.headers })
   callback(null, { statusCode: 200, body: JSON.stringify(event.headers, null, 2) })
