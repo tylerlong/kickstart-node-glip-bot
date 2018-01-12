24f19673c398a3b6829e19a4c6ddfd56b659b94c
Tyler Long <tyler4long@gmail.com>
2018-01-12 20:17:38 +0800

    Refactor code

--- messages/index.js
+++ messages/index.js
@@ -1,13 +1,14 @@
 import RingCentral from 'ringcentral-js-concise'
 
-const handleMessage = async (event, context, callback) => {
+const token = JSON.parse(process.env.GLIP_API_TOKEN)
+const rc = new RingCentral('', '', process.env.GLIP_API_SERVER)
+rc.token(token)
+
+const handleMessage = (event, context, callback) => {
   const body = JSON.parse(event.body)
   const message = body.body
-  const token = JSON.parse(process.env.GLIP_API_TOKEN)
   if (message && message.creatorId !== token.owner_id) {
-    const rc = new RingCentral('', '', process.env.GLIP_API_SERVER)
-    rc.token(token)
-    await rc.post('/restapi/v1.0/glip/posts', {
+    rc.post('/restapi/v1.0/glip/posts', {
       groupId: message.groupId,
       text: JSON.stringify(message, null, 2),
       attachments: undefined
