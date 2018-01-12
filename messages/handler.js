a87dce98cd8dc8e63b0861cbcd635ad5578c292d
Tyler Long <tyler4long@gmail.com>
2018-01-12 15:58:01 +0800

    First try of env vars

--- messages/handler.js
+++ messages/handler.js
@@ -1,6 +1,6 @@
 const handleMessage = (event, context, callback) => {
-  const body = JSON.parse(event.body)
-  callback(null, { statusCode: 200, body: JSON.stringify(body, null, 2) })
+  // const body = JSON.parse(event.body)
+  callback(null, { statusCode: 200, body: process.env.GLIP_API_SERVER + ' ' + process.env.GLIP_API_TOKEN })
 }
 
 module.exports = { handleMessage }
